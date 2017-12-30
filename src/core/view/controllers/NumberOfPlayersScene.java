3b41f52f5d832700075bc5d0a04cee5e4160cb76
BartekCupial <bartlomiej.cupial@gmail.com>
2017-12-30 23:00:18 +0100

    Dynamically changed amount of visible players ChoiceBoxes

--- src/core/view/controllers/NumberOfPlayersScene.java
+++ src/core/view/controllers/NumberOfPlayersScene.java
@@ -2,7 +2,6 @@ package core.view.controllers;
 
 import Sample.Main;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 
 import java.io.IOException;
@@ -10,13 +9,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class NumberOfPlayersScene implements Initializable {
+
     static int choice;
     public ChoiceBox<Integer> choiceBox = new ChoiceBox<>();
-    public Button NextButton =  new Button();
-    public Button BackButton = new Button();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         choiceBox.setValue(2);
         choiceBox.getItems().addAll(2,3,4,6);
     }
@@ -29,16 +28,65 @@ public class NumberOfPlayersScene implements Initializable {
         Main.mainContainer.setScene(Main.ChoosingSceneID);
     }
 
-    private static void getChoice(ChoiceBox<Integer> choiceBox){
+    private void getChoice(ChoiceBox<Integer> choiceBox){
         choice = choiceBox.getValue();
+
         if(NumberOfPlayersScene.choice == 6){
             System.out.println("six");
+            PlayersScene.playersScene.label1.setVisible(true);
+            PlayersScene.playersScene.label2.setVisible(true);
+            PlayersScene.playersScene.label3.setVisible(true);
+            PlayersScene.playersScene.label4.setVisible(true);
+            PlayersScene.playersScene.label5.setVisible(true);
+            PlayersScene.playersScene.label6.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox1.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox2.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox3.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox4.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox5.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox6.setVisible(true);
         }else if(NumberOfPlayersScene.choice == 4){
             System.out.println("four");
+            PlayersScene.playersScene.label1.setVisible(true);
+            PlayersScene.playersScene.label2.setVisible(true);
+            PlayersScene.playersScene.label3.setVisible(true);
+            PlayersScene.playersScene.label4.setVisible(true);
+            PlayersScene.playersScene.label5.setVisible(false);
+            PlayersScene.playersScene.label6.setVisible(false);
+            PlayersScene.playersScene.ChoiceBox1.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox2.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox3.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox4.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox5.setVisible(false);
+            PlayersScene.playersScene.ChoiceBox6.setVisible(false);
         }else if(NumberOfPlayersScene.choice == 3){
             System.out.println("three");
+            PlayersScene.playersScene.label1.setVisible(true);
+            PlayersScene.playersScene.label2.setVisible(true);
+            PlayersScene.playersScene.label3.setVisible(true);
+            PlayersScene.playersScene.label4.setVisible(false);
+            PlayersScene.playersScene.label5.setVisible(false);
+            PlayersScene.playersScene.label6.setVisible(false);
+            PlayersScene.playersScene.ChoiceBox1.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox2.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox3.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox4.setVisible(false);
+            PlayersScene.playersScene.ChoiceBox5.setVisible(false);
+            PlayersScene.playersScene.ChoiceBox6.setVisible(false);
         }else if(NumberOfPlayersScene.choice == 2){
             System.out.println("two");
+            PlayersScene.playersScene.label1.setVisible(true);
+            PlayersScene.playersScene.label2.setVisible(true);
+            PlayersScene.playersScene.label3.setVisible(false);
+            PlayersScene.playersScene.label4.setVisible(false);
+            PlayersScene.playersScene.label5.setVisible(false);
+            PlayersScene.playersScene.label6.setVisible(false);
+            PlayersScene.playersScene.ChoiceBox1.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox2.setVisible(true);
+            PlayersScene.playersScene.ChoiceBox3.setVisible(false);
+            PlayersScene.playersScene.ChoiceBox4.setVisible(false);
+            PlayersScene.playersScene.ChoiceBox5.setVisible(false);
+            PlayersScene.playersScene.ChoiceBox6.setVisible(false);
         }
         Main.mainContainer.setScene(Main.PlayersSceneID);
     }
--- src/core/view/controllers/PlayersScene.java
+++ src/core/view/controllers/PlayersScene.java
@@ -1,16 +1,53 @@
 package core.view.controllers;
 
 import Sample.Main;
+import core.model.Player;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class PlayersScene implements Initializable {
+    public static PlayersScene playersScene;
+    ArrayList<ChoiceBox<String>> BoxArray = new ArrayList<>();
+    ArrayList<String> choices = new ArrayList<>();
+    public ChoiceBox<String> ChoiceBox1 = new ChoiceBox<>();
+    public ChoiceBox<String> ChoiceBox2 = new ChoiceBox<>();
+    public ChoiceBox<String> ChoiceBox3 = new ChoiceBox<>();
+    public ChoiceBox<String> ChoiceBox4 = new ChoiceBox<>();
+    public ChoiceBox<String> ChoiceBox5 = new ChoiceBox<>();
+    public ChoiceBox<String> ChoiceBox6 = new ChoiceBox<>();
+    public Label label1 = new Label();
+    public Label label2 = new Label();
+    public Label label3 = new Label();
+    public Label label4 = new Label();
+    public Label label5 = new Label();
+    public Label label6 = new Label();
+
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        playersScene = this;
+        choices.add("Empty");
+        choices.add("Computer");
+        choices.add("Human");
+        BoxArray.add(ChoiceBox1);
+        BoxArray.add(ChoiceBox2);
+        BoxArray.add(ChoiceBox3);
+        BoxArray.add(ChoiceBox4);
+        BoxArray.add(ChoiceBox5);
+        BoxArray.add(ChoiceBox6);
 
+        for(int j=0; j<6; j++) {
+            for (int i = 0; i < 3; i++) {
+                BoxArray.get(j).getItems().add(choices.get(i));
+            }
+            BoxArray.get(j).setValue(choices.get(0));
+        }
     }
 
     public void NextButtonClicked(){
@@ -19,5 +56,9 @@ public class PlayersScene implements Initializable {
 
     public void BackButtonClicked(){
         Main.mainContainer.setScene(Main.NumberOfPlayersSceneID);
+    }
+
+    public void hideCheckBox(int number){
+        BoxArray.get(number).setVisible(false);
     }
 }
