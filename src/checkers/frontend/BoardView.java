57c9130696c58d5797609c46aa851f1f699009bf
Maciej Hajduk <ebrithil97@gmail.com>
2018-01-03 16:15:58 +0100

    boardview finished

--- src/checkers/frontend/BoardView.java
+++ src/checkers/frontend/BoardView.java
@@ -63,6 +63,59 @@ public class BoardView extends AbstractView implements Observer {
     }
 
     /**
+     * Overrides the paint method.
+     */
+    @Override
+    public void paint(Graphics graphics) {
+        graphics.drawImage(wallpaper, 0, 0, null);
+
+        int width = start_x;
+        int height = start_y;
+        boolean even = true;
+        int x, y;
+
+        for (y = 0; y < 17; y++) {
+            for (x = 0; x < 13; x++) {
+                if (board.getTiles()[x][y].getPawn() != null) {
+                    switch (board.player_list_getter().indexOf(board.getTiles()[x][y].getPawn().getOwner())) { //get the owner
+                        case 0:
+                            players_pawn_image = (BufferedImage) AssetsLoader.load("src/assets/pawn_blue.png", "image");
+                            break;
+                        case 1:
+                            players_pawn_image = (BufferedImage) AssetsLoader.load("src/assets/pawn_red.png", "image");
+                            break;
+                        case 2:
+                            players_pawn_image = (BufferedImage) AssetsLoader.load("src/assets/pawn_yellow.png", "image");
+                            break;
+                        case 3:
+                            players_pawn_image = (BufferedImage) AssetsLoader.load("src/assets/pawn_green.png", "image");
+                            break;
+                        case 4:
+                            players_pawn_image = (BufferedImage) AssetsLoader.load("src/assets/pawn_white.png", "image");
+                            break;
+                        case 5:
+                            players_pawn_image = (BufferedImage) AssetsLoader.load("src/assets/pawn_black.png", "image");
+                            break;
+                        default:
+                            break;
+                    }
+                    graphics.drawImage(players_pawn_image, width, height, null);
+                }
+                width += pawn_width;
+            }
+            height += pawn_height;
+            width = start_x;
+            if (even) {
+                width += (pawn_width / 2);
+                even = false;
+            } else {
+                even = true;
+            }
+        }
+        show_msg(getGraphics());
+    }
+
+    /**
      * Mouse listener - user interact with the graphical interface.
      */
     private class gridListener extends MouseAdapter {
@@ -105,6 +158,7 @@ public class BoardView extends AbstractView implements Observer {
                 tile_selected = null;
             }
         }
+
     }
 
     /**
@@ -128,16 +182,16 @@ public class BoardView extends AbstractView implements Observer {
                 return;
             }
 
-        int width = start_x + (pos_x * pawn_width);
-        int height = start_y + (pos_y * pawn_height);
+            int width = start_x + (pos_x * pawn_width);
+            int height = start_y + (pos_y * pawn_height);
 
-        //for odd row
-        if (!(pos_y % 2 == 0)) {
-            width += (pawn_width / 2);
-        }
+            //for odd row
+            if (!(pos_y % 2 == 0)) {
+                width += (pawn_width / 2);
+            }
 
-        getGraphics().drawImage(selected_image, width, height, null);
-        drawViableMoves(pos_x, pos_y);
+            getGraphics().drawImage(selected_image, width, height, null);
+            draw_all_moves(pos_x, pos_y);
         } catch (Exception ignore) { /*nothing here*/}
     }
 
@@ -147,7 +201,7 @@ public class BoardView extends AbstractView implements Observer {
      * @param pos_x the x position.
      * @param pos_y the y position.
      */
-    private void drawViableMoves(int pos_x, int pos_y) {
+    private void draw_all_moves(int pos_x, int pos_y) {
         //get all possible moves
         ArrayList<GameField[]> moves = board.findMoves(board.getTiles()[pos_x][pos_y]);
 
@@ -156,85 +210,28 @@ public class BoardView extends AbstractView implements Observer {
             return;
         }
 
-        for (GameField[] move : moves) { //iterate through all the possible moves
-            simpleMove(move[1].getX(), move[1].getY()); //and then paint them using the simpleMove method
+        for (GameField[] move : moves) {
+            //paint them
+            draw_single_move(move[1].getX(), move[1].getY());
         }
     }
 
     /**
-     * Draws a move representation, on the given tile number, by converting the tile number to the pixel values for width and height
+     * Draws a move.
      *
-     * @param pos_x the x position of the tile to paint as a viable move
-     * @param pos_y the y position of the tile to paint as a viable move
+     * @param pos_x the x position
+     * @param pos_y the y position
      */
-    private void simpleMove(int pos_x, int pos_y) {
-        int wid = start_x + (pos_x * pawn_width); //convert the tile numbers to pixel values
-        int hig = start_y + (pos_y * pawn_height);
+    private void draw_single_move(int pos_x, int pos_y) {
+        //convert the tile numbers to pixel values
+        int width = start_x + (pos_x * pawn_width);
+        int height = start_y + (pos_y * pawn_height);
 
-        if (!(pos_y % 2 == 0)) { //if it's on an even row
-            wid += (pawn_width / 2); //then we shift it over half a space
+        //for odd row
+        if (!(pos_y % 2 == 0)) {
+            width += (pawn_width / 2);
         }
-
-        getGraphics().drawImage(available_move, wid, hig, null); //performs the actual drawing, using the pixel values
-    }
-
-    /**
-     * Overrides the paint method of JComponent, to paint the necessities needed for the Checkers AbstractBoard
-     *
-     * @param g The graphics object used to paint the images required
-     */
-    @Override
-    public void paint(Graphics g) {
-        g.drawImage(wallpaper, 0, 0, null); //paint the wallpaper image at the top left pixel
-
-        //paint pieces start
-        int wid = start_x;
-        int hig = start_y;
-        boolean ev = true; //placeholder value denoting whether currently on a even row (0,2 etc)
-
-        int y;
-        int x;
-
-        for (y = 0; y < 17; y++) { //iterate through the board
-            for (x = 0; x < 13; x++) {
-                if (board.getTiles()[x][y].getPawn() != null) { //if there is a piece in the tile
-                    switch (board.player_list_getter().indexOf(board.getTiles()[x][y].getPawn().getOwner())) { //get the owner
-                        case 0:
-                            players_pawn_image = (BufferedImage) AssetsLoader.load("src/assets/pawn_blue.png", "image");
-                            break;
-                        case 1:
-                            players_pawn_image = (BufferedImage) AssetsLoader.load("src/assets/pawn_red.png", "image");
-                            break;
-                        case 2:
-                            players_pawn_image = (BufferedImage) AssetsLoader.load("src/assets/pawn_yellow.png", "image");
-                            break;
-                        case 3:
-                            players_pawn_image = (BufferedImage) AssetsLoader.load("src/assets/pawn_green.png", "image");
-                            break;
-                        case 4:
-                            players_pawn_image = (BufferedImage) AssetsLoader.load("src/assets/pawn_white.png", "image");
-                            break;
-                        case 5:
-                            players_pawn_image = (BufferedImage) AssetsLoader.load("src/assets/pawn_black.png", "image");
-                            break;
-                        default:
-                            break;
-                    }
-                    g.drawImage(players_pawn_image, wid, hig, null); //and then paint the piece
-                }
-                wid += pawn_width; //add to the pixel value for the width on which we paint pieces
-            }
-            hig += pawn_height; //and add to the height value for where we paint
-            wid = start_x; //then reset the width value since we exited the inner loop
-            if (ev) { //if we're on an even row, then we need to shift the tile over half the width of a tile
-                wid += (pawn_width / 2);
-                ev = false; //and then reset it for the next row
-            } else { //if we're not on an even row then we only need to reset the variable
-                ev = true;
-            }
-        }
-        //paint pieces end
-        show_msg(getGraphics()); //now that we've repainted, we redraw the message in case it's also changed
+        getGraphics().drawImage(available_move, width, height, null);
     }
 
     /**
@@ -247,7 +244,7 @@ public class BoardView extends AbstractView implements Observer {
             for (int x = 0; x < 13; x++) {
                 GamePawn piece = board.getTiles()[x][y].getPawn();
                 if (piece != null && piece.getOwner() == board.active_player_getter()) {
-                    drawViableMoves(x, y);
+                    draw_all_moves(x, y);
                 }
             }
         }
